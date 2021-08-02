9a17cef62fc95778987376f23033d2dc23018138
issueyh <issueyh12921@gmail.com>
2021-08-02 15:07:11 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
+require('./config/mongoose')
 const app = express()
 const port = 3000
 
@@ -8,18 +8,6 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
-
-mongoose.connect('mongodb://localhost/url-shortener', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-    console.log('mongodb error!')
-})
-
-db.once('open', () => {
-    console.log('mongodb connected!')
-})
 
 app.get('/', (req, res) => {
     res.render('index')
